3ee5f9b32d34dda556c1229396b2e52ea85682a8
Alex Barongo <alexbarongo1@gmail.com>
2023-01-20 18:03:01 +0000

    left a msg on a feature to work on

--- dist/app.js
+++ dist/app.js
@@ -1,79 +1,77 @@
-const toggleSwitch = document.querySelector(".round")
-const promoArcade = document.querySelector(".plan-to-choose .promo-arcade")
-const promoAdvanced = document.querySelector(".plan-to-choose .promo-advanced")
-const promoPro = document.querySelector(".plan-to-choose .promo-pro")
-const monthly = document.querySelector(".month")
-const yearly = document.querySelector(".year")
-const plans = document.querySelectorAll(".plan-to-choose")
-const arcade = document.querySelector(".arcade")
-const advanced = document.querySelector(".advanced")
-const pro = document.querySelector(".pro") 
-const selections = document.querySelector(".selections")
-const serviceAddOns = document.querySelector(".service")
-const storageAddOns = document.querySelector(".storage")
-const profileAddOns = document.querySelector(".profile")
-const numberSteps = document.querySelectorAll(".steps h2")
-const steps = document.querySelectorAll(".container")
+const toggleSwitch = document.querySelector(".round");
+const promoArcade = document.querySelector(".plan-to-choose .promo-arcade");
+const promoAdvanced = document.querySelector(".plan-to-choose .promo-advanced");
+const promoPro = document.querySelector(".plan-to-choose .promo-pro");
+const monthly = document.querySelector(".month");
+const yearly = document.querySelector(".year");
+const plans = document.querySelectorAll(".plan-to-choose");
+const arcade = document.querySelector(".arcade");
+const advanced = document.querySelector(".advanced");
+const pro = document.querySelector(".pro");
+const selections = document.querySelector(".selections");
+const serviceAddOns = document.querySelector(".service");
+const storageAddOns = document.querySelector(".storage");
+const profileAddOns = document.querySelector(".profile");
+const numberSteps = document.querySelectorAll(".steps h2");
+const steps = document.querySelectorAll(".container");
 
+toggleSwitch.addEventListener("click", switchToggle);
 
-
-
-toggleSwitch.addEventListener("click", switchToggle)
-
-function switchToggle(){
-	if(toggleSwitch.classList.contains("round-clicked")){
-		toggleSwitch.classList.remove("round-clicked")
-		toggleSwitch.classList.add("round")
-		monthly.style.color = "hsl(213, 96%, 18%)"
-		yearly.style.color = "hsl(231, 11%, 63%)"
-		promoArcade.style.display = "none"
-		promoAdvanced.style.display = "none"
-		promoPro.style.display = "none"
-		arcade.textContent = "$9/mo"
-		advanced.textContent = "$12/mo"
-		pro.textContent = "$15/mo"
-		serviceAddOns.textContent = "$1/mo"
-		storageAddOns.textContent = "$2/mo"
-		profileAddOns.textContent = "$2/mo"
-	} else {
-		monthly.style.color = "hsl(231, 11%, 63%)"
-		yearly.style.color = "hsl(213, 96%, 18%)"
-		toggleSwitch.classList.remove("round")
-		toggleSwitch.classList.add("round-clicked")
-		promoArcade.style.display = "block"
-		promoAdvanced.style.display = "block"
-		promoPro.style.display = "block"
-		toggleSwitch.style.transition = "0.2s ease"
-		arcade.textContent = "$90/yr"
-		advanced.textContent = "$120/yr"
-		pro.textContent = "$150/yr"
-		serviceAddOns.textContent = "$10/yr"
-		storageAddOns.textContent = "$20/yr"
-		profileAddOns.textContent = "$20/yr"
-	}
+function switchToggle() {
+  if (toggleSwitch.classList.contains("round-clicked")) {
+    toggleSwitch.classList.remove("round-clicked");
+    toggleSwitch.classList.add("round");
+    monthly.style.color = "hsl(213, 96%, 18%)";
+    yearly.style.color = "hsl(231, 11%, 63%)";
+    promoArcade.style.display = "none";
+    promoAdvanced.style.display = "none";
+    promoPro.style.display = "none";
+    arcade.textContent = "$9/mo";
+    advanced.textContent = "$12/mo";
+    pro.textContent = "$15/mo";
+    serviceAddOns.textContent = "$1/mo";
+    storageAddOns.textContent = "$2/mo";
+    profileAddOns.textContent = "$2/mo";
+  } else {
+    monthly.style.color = "hsl(231, 11%, 63%)";
+    yearly.style.color = "hsl(213, 96%, 18%)";
+    toggleSwitch.classList.remove("round");
+    toggleSwitch.classList.add("round-clicked");
+    promoArcade.style.display = "block";
+    promoAdvanced.style.display = "block";
+    promoPro.style.display = "block";
+    toggleSwitch.style.transition = "0.2s ease";
+    arcade.textContent = "$90/yr";
+    advanced.textContent = "$120/yr";
+    pro.textContent = "$150/yr";
+    serviceAddOns.textContent = "$10/yr";
+    storageAddOns.textContent = "$20/yr";
+    profileAddOns.textContent = "$20/yr";
+  }
 }
 
-function planChoises(){
-	for (let i = 0; i < plans.length; i++) {
-		plans[i].addEventListener("click", function(){
-			if(i === 0 || i === 1 || i === 2){
-				plans[i].style.border = "1px solid hsl(243, 100%, 62%)"
-			}
-		})
-	}
+function planChoises() {
+  for (let i = 0; i < plans.length; i++) {
+    plans[i].addEventListener("click", function () {
+      if (i === 0 || i === 1 || i === 2) {
+        plans[i].style.border = "1px solid hsl(243, 100%, 62%)";
+      }
+    });
+  }
 }
 
-planChoises()
+planChoises();
 
+//create a function that changes the background
+//of  h2 element hwen the corresonding step is active
 
-function stepIndicator(){
-	for(let i = 0; i < numberSteps.length; i++){
-		numberSteps[i].style.background = "hsl(206, 94%, 87%)"
-		numberSteps[i].style.color = "hsl(213, 96%, 18%)"
-		numberSteps[i].style.border = "1px solid hsl(213, 96%, 18%)"
-	}
+//this stepIndicator function changes to all the h2s.. it ain't 'posed do that
+function stepIndicator() {
+  for (let i = 0; i < numberSteps.length; i++) {
+    numberSteps[i].style.background = "hsl(206, 94%, 87%)";
+    numberSteps[i].style.color = "hsl(213, 96%, 18%)";
+    numberSteps[i].style.border = "1px solid hsl(213, 96%, 18%)";
+  }
 }
 
-
-stepIndicator()
-
+stepIndicator();
